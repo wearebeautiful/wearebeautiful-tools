1d06300f3cbb2ad9610a7d05fd437dbc88465736
Robert Kaye <rob@musicbrainz.org>
2019-07-25 21:49:12 +0200

    Sanity checking of incoming bundles is complete.

--- wearebeautiful/admin.py
+++ wearebeautiful/admin.py
@@ -1,8 +1,13 @@
 import os
-from flask import render_template, current_app, redirect, Blueprint
+import zipfile
+from flask import render_template, current_app, redirect, Blueprint, request
+from werkzeug.exceptions import BadRequest
+import tempfile
 import config
 import json
+from io import StringIO
 from wearebeautiful.auth import _auth as auth
+#from wearebeautiful.bundle import sanity_check_bundle
 
 bp = Blueprint('admin', __name__)
 
@@ -30,4 +35,36 @@ def admin_upload_get():
 @bp.route('/upload', methods=['POST'])
 @auth.login_required
 def admin_upload_post():
+
+    allowed_files = ['manifest.json', 'surface-low.obj', 'surface-medium.obj', 'solid.obj', 'surface-orig.obj', 'screenshot.jpg']
+
+    if 'file' not in request.files:
+        raise BadRequest("Request is missing file part.")
+
+    try: 
+        f = request.files['file']
+        filename = tempfile.NamedTemporaryFile()
+        f.save(filename.name)
+    except IOError as err:
+        if filename.name:
+            try:
+                os.unlink(filename.name)
+            except Exception:
+                pass
+        raise InternalServerError("Cannot save bundle to disk: ", err)
+
+    try:
+        zipf = zipfile.ZipFile(filename.name)
+    except zipfile.BadZipFile:
+        raise BadRequest("Invalid zip file.")
+
+    files = zipf.namelist()
+    if allowed_files != files:
+        raise BadRequest("Incorrect files in the bundle.")
+
+    try:
+        os.unlink(filename.name)
+    except Exception:
+        pass
+
     return ""
