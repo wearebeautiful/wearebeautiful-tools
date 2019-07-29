5d6af630ec7fea48d5d06ede19a91235a2c9e25f
Robert Kaye <rob@musicbrainz.org>
2019-07-29 14:08:55 +0100

    Change upload max file size

--- start-containers.sh
+++ start-containers.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 docker run -d \
     --expose 3031 \
     --name wearebeautiful.info-beta \
     -v wab-bundles:/code/wearebeautiful.info/static/bundle \
+    -v $SRC_DIR/admin/nginx/vhost.d:/etc/nginx/vhost.d:ro \
     --env "VIRTUAL_HOST=wearebeautiful.info" \
     --env "LETSENCRYPT_HOST=wearebeautiful.info" \
     --env "LETSENCRYPT_EMAIL=mayhem@gmail.com" \
