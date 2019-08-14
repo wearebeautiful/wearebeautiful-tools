043b80fd503baa0d766cc64cc1bd6e1eac1cfa5e
Robert Kaye <rob@musicbrainz.org>
2019-08-14 15:54:23 +0200

    Formatting improvements

--- wearebeautiful/model_params.py
+++ wearebeautiful/model_params.py
@@ -3,11 +3,96 @@ SURFACE_FILE = "surface.stl"
 SCREENSHOT_FILE = "screenshot.jpg"
 MANIFEST_FILE = "manifest.json"
 
+MIN_FREETEXT_FIELD_LEN = 5
 FORMAT_VERSION = 1 
-REQUIRED_KEYS = [ "version", "id", "created", "gender", "country", "age", "body_type", 
-                  "mother", "ethnicity", "released", "bodypart", "pose"]
-OPTIONAL_KEYS = [ "pose_variant", "gender_comment", "comment", "other", "modification" ]
-GENDERS = ["female", "male", "trans-mtf", "trans-ftm", "other"]
+
+REQUIRED_KEYS = [ 
+    "version", 
+    "id", 
+    "created", 
+    "gender", 
+    "country", 
+    "age", 
+    "body_type", 
+    "body_part", 
+    "mother", 
+    "ethnicity", 
+    "released", 
+    "pose"
+]
+
+OPTIONAL_KEYS = [ 
+    "gender_comment", 
+    "comment", 
+    "other", 
+    "modification" 
+]
+
+GENDERS = [
+    "female", 
+    "male", 
+    "trans-mtf", 
+    "trans-ftm", 
+    "other"
+]
+
+BODY_TYPES = [
+    "thin", 
+    "fit", 
+    "full", 
+    "overweight"
+]
+
+BODY_PART = [ 
+    { "body" : "Y" }, 
+    { "breast" : "B" }, 
+    { "bust" : "S" }, 
+    { "buttocks" : "U" }, 
+    { "nipple" : "N" }, 
+    { "penis" : "P" }, 
+    { "torso" : "T" }, 
+    { "vulva" : "V" }, 
+]
+
+EXCITED = [
+    "not excited", 
+    "excited", 
+    "partially excited"
+]
+
+ARRANGEMENT = [ 
+    { "spread" : "S" }, 
+    { "retracted" : "R" }, 
+    { "arranged" : "A" }, 
+    { "natural" : "N" }, 
+]
+
+POSE = [ 
+    { "standing" : "S" }, 
+    { "sitting" : "T" }, 
+    { "lying" : "L" }, 
+]
+
+MODIFICATIONS = [ 
+    "pregnancy", 
+    "nursed", 
+    "circumcised", 
+    "augmented", 
+    "episiotomy",
+    "masectomy", 
+    "labiaplasty", 
+    "female-to-male", 
+    "male-to-female", 
+    "fgm", 
+]
+
+MOTHER = [
+    "no", 
+    "vaginal", 
+    "caesarean"
+]
+
+
 COUNTRIES = [ "AF", "AL", "DZ", "Sa", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY",
 "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", "BG", "BF", "BI", "CV", "KH", "CM", "CA",
 "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CD", "CG", "CK", "CR", "HR", "CU", "CW", "CY", "CZ", "CI", "DK",
@@ -21,9 +106,3 @@ COUNTRIES = [ "AF", "AL", "DZ", "Sa", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", 
 "SX", "SK", "SI", "SB", "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH",
 "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "UM", "US", "UY", "UZ", "VU", "VE",
 "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW", "AX" ]
-BODY_TYPES = ["thin", "fit", "full", "overweight"]
-MIN_FREETEXT_FIELD_LEN = 5
-MODIFICATIONS = ["none", "circumcised"," fgm", "labiaplasty", "masectomy", "female-to-male", "male-to-female", "breastfeeding", "pregnant", "episiotomy"]
-MOTHER = ["no", "vaginal", "caesarean"]
-BODYPART = [ "body", "bust", "breast", "nipple", "vulva", "buttocks", "penis" ]
-POSE = ["normal", "aroused", "variant"]
